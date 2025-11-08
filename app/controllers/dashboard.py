f836e195d52aff2d23901f01ac5b7a4d34efa76d
Amir <amir0703@gmail.com>
2025-11-08 23:47:00 +0800

    Added graphs

--- app/controllers/dashboard.py
+++ app/controllers/dashboard.py
@@ -7,40 +7,74 @@ from models.book import Booking
 dashboard = Blueprint('dashboard', __name__)
 
 @dashboard.route('/trend_chart', methods=['GET', 'POST'])
+@login_required
 def trend_chart():
+    if current_user.email != "admin@abc.com":
+        return "Access denied. Admins only.", 403
+    else:
+        if request.method == 'GET':
+            return render_template('trend_chart.html', panel="Package Chart")
     
-    if request.method == 'GET':
-        
-        #I want to get some data from the service
-        return render_template('trend_chart.html', panel="Package Chart")    #do nothing but to show index.html
-    
-    elif request.method == 'POST':
-        
-        #Chart is indexed by first date and last date
-        #And we are going to plot the period from 2021-01-17 to 2021-01-23
+        elif request.method == 'POST':
+            chart_type = request.form.get('chart_type', 'amount_incoming')
+            all_bookings = Booking.getAllBookings()
+            print(f"There are {len(all_bookings)} of booking records")
 
-        #Trend is reconstructed each time from Booking to incorporate new booking since the last trend chart
+            if chart_type == 'amount_incoming':
+                # Process amount incoming data
+                hotel_costbyDate = {}
 
-        all_bookings = Booking.getAllBookings()
-        print(f"There are {len(all_bookings)} of booking records")
-        #TREND.objects.delete()
-
-        # hotel_costbyDate[hotel_name] = {date: accum_cost, ...}
-        hotel_costbyDate = {}
-
-        for aBooking in list(all_bookings):
-            hotel_name = aBooking.package.hotel_name
-            check_in_date = aBooking.check_in_date
+                for aBooking in list(all_bookings):
+                    hotel_name = aBooking.package.hotel_name
+                    check_in_date = aBooking.check_in_date
                       
-            if hotel_name not in hotel_costbyDate:
-                hotel_costbyDate[hotel_name] = {}
-            if check_in_date not in hotel_costbyDate[hotel_name]:
-                hotel_costbyDate[hotel_name][check_in_date] = 0
-            hotel_costbyDate[hotel_name][check_in_date] += aBooking.total_cost
+                    if hotel_name not in hotel_costbyDate:
+                        hotel_costbyDate[hotel_name] = {}
+                    if check_in_date not in hotel_costbyDate[hotel_name]:
+                        hotel_costbyDate[hotel_name][check_in_date] = 0
+                    hotel_costbyDate[hotel_name][check_in_date] += aBooking.total_cost
         
-        hotel_costbyDateSortedListValues = {}
-        for hotel, dateAmts in hotel_costbyDate.items():
-            hotel_costbyDateSortedListValues[hotel] = sorted(list(dateAmts.items()))
+                hotel_costbyDateSortedListValues = {}
+                for hotel, dateAmts in hotel_costbyDate.items():
+                    hotel_costbyDateSortedListValues[hotel] = sorted(list(dateAmts.items()))
 
-        return jsonify({'chartDim': hotel_costbyDateSortedListValues, 'labels': []})    
+                print("Sending Amount Incoming data:", hotel_costbyDateSortedListValues)
+                return jsonify({'chartDim': hotel_costbyDateSortedListValues, 'labels': [], 'chartType': 'line'})
 
+            elif chart_type == 'bookings_by_month':
+                month_hotel_bookings = {}
+    
+                for aBooking in list(all_bookings):
+                    hotel_name = aBooking.package.hotel_name
+                    check_in_date = aBooking.check_in_date
+        
+                    month_year = check_in_date.strftime("%B %Y")
+        
+                    if month_year not in month_hotel_bookings:
+                        month_hotel_bookings[month_year] = {}
+                    if hotel_name not in month_hotel_bookings[month_year]:
+                        month_hotel_bookings[month_year][hotel_name] = 0
+                    month_hotel_bookings[month_year][hotel_name] += 1  # Count bookings
+    
+                all_hotels = set()
+                for month_data in month_hotel_bookings.values():
+                    all_hotels.update(month_data.keys())
+                all_hotels = sorted(list(all_hotels))
+    
+                all_months = sorted(list(month_hotel_bookings.keys()), 
+                                   key=lambda x: datetime.strptime(x, "%B %Y"))
+    
+                month_bookings_data = {}
+                for month in all_months:
+                    month_data = []
+                    for hotel in all_hotels:
+                        count = month_hotel_bookings[month].get(hotel, 0)
+                        month_data.append([hotel, count])  # [hotel_name, booking_count]
+                    month_bookings_data[month] = month_data
+    
+                print("Sending Bookings By Month data:", month_bookings_data)
+                return jsonify({
+                    'chartDim': month_bookings_data, 
+                    'labels': all_hotels,  # Hotel names for x-axis
+                    'chartType': 'bar'
+                })
